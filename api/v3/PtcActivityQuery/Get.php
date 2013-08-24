ba65b843c82c692d6e706d77377ce1bb175713bd
mmikitka <matt@mikitka.net>
2013-08-23 20:44:48 -0400

    Completed target_contact entity; added where condition to every table

--- api/v3/PtcActivityQuery/Get.php
+++ api/v3/PtcActivityQuery/Get.php
@@ -27,12 +27,39 @@ function _civicrm_api3_ptc_activity_query_get_spec(&$spec) {
  * @throws API_Exception
  */
 function civicrm_api3_ptc_activity_query_get($params) {
+  // The API result that will be returned to the caller
   $return_values = array();
+
+  // Configuration information for all non-custom tables referenced
+  // by the SQL query.
+  $tbl_configs = _ptc_get_table_configs();
+
+  // Map to mark whether a table has been added to the JOIN
+  // Initialize the map with necessary tables
+  $tbl_added = array(
+    'civicrm_activity' => TRUE,
+    'civicrm_activity_assignment' => TRUE,
+    'civicrm_activity_target' => TRUE,
+  );
 
   // Entities to return in the result
   $return_entities = array();
   if (!empty($params['entities'])) {
     $return_entities = explode(',', CRM_Utils_Array::value('entities', $params, ''));
+  }
+
+  // Add the requested entity tables to the result set
+  foreach ($return_entities as $entity) {
+    switch ($entity) {
+      case 'target_contact':
+        $tbl_added['civicrm_contact'] = TRUE;
+        $tbl_added['civicrm_phone'] = TRUE;
+        $tbl_added['civicrm_email'] = TRUE;
+        break;
+
+      default:
+        break;
+    }
   }
 
   // Fields to return in the result
@@ -61,18 +88,6 @@ function civicrm_api3_ptc_activity_query_get($params) {
       }
     }
   }
-
-  // Configuration information for all non-custom tables referenced
-  // by the SQL query.
-  $tbl_configs = _ptc_get_table_configs();
-
-  // Map to mark whether a table has been added to the JOIN
-  // Initialize the map with necessary tables
-  $tbl_added = array(
-    'civicrm_activity' => TRUE,
-    'civicrm_activity_assignment' => TRUE,
-    'civicrm_activity_target' => TRUE,
-  );
 
   // Store the filter conditions
   $filter = array();
@@ -105,6 +120,7 @@ function civicrm_api3_ptc_activity_query_get($params) {
           'entity' => 'relationships',
           'join_type' => "LEFT",
           'join_condition' => "civicrm_activity_target.target_contact_id = civicrm_relationship.{$join_col}",
+          'where_condition' => NULL,
         );
         $tbl_added['civicrm_relationship'] = TRUE;
       }
@@ -142,6 +158,7 @@ function civicrm_api3_ptc_activity_query_get($params) {
               'entity' => 'target_contact',
               'join_type' => "LEFT",
               'join_condition' => "civicrm_activity_target.target_contact_id = {$custom_group_tbl}.entity_id",
+              'where_condition' => NULL,
             );
             $tbl_added[$custom_group_tbl] = TRUE;
           } elseif ($custom_group_extends == 'Activity') {
@@ -151,6 +168,7 @@ function civicrm_api3_ptc_activity_query_get($params) {
               'entity' => 'activity',
               'join_type' => "LEFT",
               'join_condition' => "civicrm_activity.id = {$custom_group_tbl}.entity_id",
+              'where_condition' => NULL,
             );
             $tbl_added[$custom_group_tbl] = TRUE;
           } else {
@@ -191,6 +209,7 @@ function civicrm_api3_ptc_activity_query_get($params) {
               'entity' => 'target_contact',
               'join_type' => "LEFT",
               'join_condition' => "civicrm_activity_target.target_contact_id = {$custom_group_tbl}.entity_id",
+              'where_condition' => NULL,
             );
             $tbl_added[$custom_group_tbl] = TRUE;
           } elseif ($custom_group_extends == 'Activity') {
@@ -200,6 +219,7 @@ function civicrm_api3_ptc_activity_query_get($params) {
               'entity' => 'activity',
               'join_type' => "LEFT",
               'join_condition' => "civicrm_activity.id = {$custom_group_tbl}.entity_id",
+              'where_condition' => NULL,
             );
             $tbl_added[$custom_group_tbl] = TRUE;
           } else {
@@ -233,14 +253,13 @@ function civicrm_api3_ptc_activity_query_get($params) {
         $sql .= ", {$tbl_name}.{$col}";
       }
 
-      // Map this field to its entity
-      $field_entity_map[$col] = $tbl_config['entity'];
-
-      // Add the alias
+      // Map this field to its entity; use the alias if available
       if (isset($col_aliases[$col])) {
         $alias = $col_aliases[$col];
         $sql .= " AS " . $alias;
         $field_entity_map[$alias] = $tbl_config['entity'];
+      } else {
+        $field_entity_map[$col] = $tbl_config['entity'];
       }
     }
   }
@@ -255,9 +274,23 @@ function civicrm_api3_ptc_activity_query_get($params) {
   }
 
   // Generate the WHERE clause
+
+  // Check whether any of the added tables require a filter
+  foreach ($tbl_added as $tbl_name => $status) {
+    if ($tbl_configs[$tbl_name]['where_condition'] !== NULL) {
+      $filter[] = $tbl_configs[$tbl_name]['where_condition'];
+    }
+  }
+
   if (count($filter) > 0) {
     $sql .= " WHERE ";
     $sql .= implode(" AND ", $filter);
+  }
+
+  // Generate SORT
+  $sequential = CRM_Utils_Array::value('sequential', $params, FALSE);
+  if ($sequential) {
+    $sql .= " ORDER BY civicrm_activity.id";
   }
 
   // Generate LIMIT
@@ -336,6 +369,7 @@ function _ptc_get_table_configs() {
     'entity' => 'activity',
     'join_type' => NULL,
     'join_condition' => NULL,
+    'where_condition' => NULL,
   );
 
   // civicrm_activity_assignment
@@ -347,6 +381,7 @@ function _ptc_get_table_configs() {
     'entity' => 'activity',
     'join_type' => "LEFT",
     'join_condition' => "civicrm_activity.id = civicrm_activity_assignment.activity_id",
+    'where_condition' => NULL,
   );
 
   // civicrm_activity_target
@@ -358,15 +393,67 @@ function _ptc_get_table_configs() {
     'entity' => 'activity',
     'join_type' => "LEFT",
     'join_condition' => "civicrm_activity.id = civicrm_activity_target.activity_id",
+    'where_condition' => NULL,
   );
 
   // civicrm_contact
   $tbl_configs['civicrm_contact'] = array(
-    'cols' => array(),
-    'col_aliases' => array(),
+    'cols' => array(
+      'id',
+      'contact_type',
+      'contact_sub_type',
+      'do_not_email',
+      'do_not_phone',
+      'do_not_mail',
+      'do_not_sms',
+      'is_opt_out',
+      'display_name',
+      'first_name',
+      'last_name',
+      'gender_id',
+      'created_date',
+      'modified_date',
+    ),
+    'col_aliases' => array(
+      'id' => 'contact_id',
+      'created_date' => 'contact_created_date',
+      'modified_date' => 'contact_modified_date',
+    ),
     'entity' => 'target_contact',
     'join_type' => "LEFT",
     'join_condition' => "civicrm_activity_target.target_contact_id = civicrm_contact.id",
+    'where_condition' => NULL,
+  );
+
+  // civicrm_phone
+  $tbl_configs['civicrm_phone'] = array(
+    'cols' => array(
+      'id',
+      'phone',
+      'phone_type_id',
+    ),
+    'col_aliases' => array(
+      'id' => 'phone_id',
+    ),
+    'entity' => 'target_contact',
+    'join_type' => "LEFT",
+    'join_condition' => "civicrm_activity_target.target_contact_id = civicrm_phone.contact_id",
+    'where_condition' => "civicrm_phone.is_primary = 1",
+  );
+
+  // civicrm_email
+  $tbl_configs['civicrm_email'] = array(
+    'cols' => array(
+      'id',
+      'email',
+    ),
+    'col_aliases' => array(
+      'id' => 'email_id',
+    ),
+    'entity' => 'target_contact',
+    'join_type' => "LEFT",
+    'join_condition' => "civicrm_activity_target.target_contact_id = civicrm_email.contact_id",
+    'where_condition' => "civicrm_email.is_primary = 1",
   );
 
   return $tbl_configs;
